7e7eac1002b3f2e11665b6e23cb038d4aacbd9ba
dongnp <dong26k@gmail.com>
2022-10-06 16:29:53 +0700

    fix nut

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,7 +13,7 @@ class TodoItem extends React.Component {
                 <span className={completed ? "completed" : null}>
                     {title}
                 </span>
-                <button className="btn-style" onClick={() => this.props.editTodo(id)}>Edit </button>
+                <button className="btn-style-edit" onClick={() => this.props.editTodo(id)}>Edit </button>
                 <button className="btn-style" onClick={() => this.props.deleteTodo(id)}> X </button>
             </li>
         );
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
     render() {
         return (
             <header className="header-container">
-                <h1 className="header-title">Simple Todo App</h1>
+                <h1 className="header-title">Todo App</h1>
             </header>
 
         );
